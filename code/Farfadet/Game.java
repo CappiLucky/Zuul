51e180a46c6af192adcba1d793a827d3b4d73a7b
CappiLucky <manon.hermann@edu.esiee.fr>
2020-02-05 22:20:23 +0100

    repararation, compillation normale possible

--- code/Farfadet/Game.java
+++ code/Farfadet/Game.java
@@ -10,11 +10,17 @@ public class Game
     private UserInterface aGui;
     private GameEngine aEngine;
 
+
+    public static void main (String[] args) { 
+	    Game jeu = new Game();
+    }
+
+
     /**
      * Begin a new game
      * Create the game and initialise its internal map.
      */
-    public Game() 
+    public Game()
     {
        this.aEngine = new GameEngine();
        this.aGui = new UserInterface(aEngine);
